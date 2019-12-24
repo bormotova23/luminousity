61bb7672d38c4021a726c1e3c53bd952a95f063f
victory-ia <58079940+victory-ia@users.noreply.github.com>
2019-12-24 18:18:53 +0200

    Create code to functions.php

---
+++ code to functions.php
@@ -0,0 +1,19 @@
+/**
+ * Process location
+ *
+ * @return void
+ */
+function cb_child_process_location( $location = null ) {
+
+	if ( ! function_exists( 'jet_theme_core' ) ) {
+		return false;
+	}
+	if( ! defined( 'ELEMENTOR_VERSION' ) ) {
+		return false;
+	}
+
+	$done = jet_theme_core()->locations->do_location( $location );
+
+	return $done;
+
+}
